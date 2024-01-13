94ad6b8c1d70f4a92f1417422366ed6c88dfafc7
lomakina0612 <lomakina0612@gmail.com>
2024-01-13 18:21:34 +0100

    Probe_16

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,8 @@ from .group import Group
 from .professor import Professor
 from .professorsubject import ProfessorSubject
 from .grade import Grade
+from sqlalchemy.orm import relationship
+
+# Add relationships after all classes are defined
+Student.group = relationship("Group", back_populates="students")
+Group.students = relationship("Student", back_populates="group")
