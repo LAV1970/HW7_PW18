81fb01f392a8a5dcf0cbc1d2adea35d3e10a107a
lomakina0612 <lomakina0612@gmail.com>
2024-01-13 18:45:14 +0100

    Proba_18

--- seed.py
+++ seed.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from faker import Faker
 import random
 from models import Student, Group, Professor, ProfessorSubject, Grade
 from datetime import date
 
-# Замените 'sqlite:///F:/Projects/Python_projects/Alex/HW7_PW18/uni_hw7.db' на ваше соединение с базой данных
 engine = create_engine("sqlite:///F:/Projects/Python_projects/Alex/HW7_PW18/uni_hw7.db")
 
 # Создаем сессию
@@ -29,9 +28,17 @@ def add_random_student_to_db():
     session.add(student)
 
 
+# Добавьте отношения после определения всех классов
+Student.group = relationship("Group", back_populates="students")
+Group.students = relationship("Student", back_populates="group")
+
 # Добавление 40 случайных студентов в базу данных
 for _ in range(40):
     add_random_student_to_db()
+
+# Добавьте отношения после определения всех классов
+Student.group = relationship("Group", back_populates="students")
+Group.students = relationship("Student", back_populates="group")
 
 # Сохраняем изменения в базе данных
 session.commit()
