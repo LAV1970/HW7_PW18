635fa6de055e137b9a8f9952d112c3c1c4417a4a
lomakina0612 <lomakina0612@gmail.com>
2024-01-14 12:05:56 +0100

    Probe_1401_2

--- seed.py
+++ seed.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 from faker import Faker
 import random
 from models import Group, Student
