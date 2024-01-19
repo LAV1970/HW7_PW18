1cab4dc2c66704f5d529cd37fe90d312c836503b
lomakina0612 <lomakina0612@gmail.com>
2024-01-19 10:45:10 +0100

    Probe_1901_3

--- alembic/env.py
+++ alembic/env.py
@@ -16,7 +16,7 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    fileConfig(os.path.join(os.path.dirname(__file__), "alembic.ini"))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
