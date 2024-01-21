5865a5a936ec47cc45fadefe171e2d51a374e4d6
lomakina0612 <lomakina0612@gmail.com>
2024-01-21 12:57:22 +0100

    2101_end

--- alembic/versions/20240121_01_add_group_id_to_students.py
+++ alembic/versions/20240121_01_add_group_id_to_students.py
@@ -18,10 +18,19 @@ revision_id = "20240121_01"
 
 # Изменения вперед (при обновлении)
 def upgrade():
-    op.add_column("students", sa.Column("group_id", sa.Integer(), nullable=True))
-    op.create_foreign_key(
-        "fk_students_group_id", "students", "groups", ["group_id"], ["id"]
-    )
+    # Проверяем, существует ли колонка "group_id" перед её добавлением
+    conn = op.get_bind()
+    inspector = sa.inspect(conn)
+    columns = inspector.get_columns("students")
+    group_id_exists = any(column["name"] == "group_id" for column in columns)
+
+    if not group_id_exists:
+        op.add_column("students", sa.Column("group_id", sa.Integer(), nullable=True))
+        op.create_foreign_key(
+            "fk_students_group_id", "students", "groups", ["group_id"], ["id"]
+        )
+    else:
+        print("Column 'group_id' already exists in table 'students'. Skipping...")
 
 
 # Изменения назад (при откате)
